65c3e3cdbf0f6528fc3a6e22eaab0a7344fb1e4a
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-11-04 15:22:15 +0100

    chore: remove toast

--- web/src/features/cards/hooks/use-update-card.ts
+++ web/src/features/cards/hooks/use-update-card.ts
@@ -1,16 +1,10 @@
 import { appQuery } from '@/api-client/api-client'
-import { notifications } from '@mantine/notifications'
 
 export function useUpdateCard() {
   return appQuery.useMutation('patch', '/v1/cards/{cardId}', {
     onSuccess: (_, __, ___, { client }) => {
       client.invalidateQueries({
         queryKey: ['get', '/v1/boards/{boardId}'],
-      })
-      notifications.show({
-        title: 'Success',
-        message: 'Task updated successfully',
-        color: 'green',
       })
     },
   })
